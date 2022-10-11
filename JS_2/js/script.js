09c11f4ac6ba0211c56e1ca56983b9db3cb98e10
Khalid <114188229+sunsetofmoon@users.noreply.github.com>
2022-10-11 14:15:48 +0300

    Add files via upload

---
+++ JS_2/js/script.js
@@ -0,0 +1,41 @@
+const myBirthday = '08.01.2006'; //создали константу , используем тип данных string
+// при попытке изменить содержимое в const выдает ошибку
+
+console.log(myBirthday);
+
+
+let p = 3.14;
+                  // используем тип данных number
+console.log(p);
+
+
+let name = prompt('Name');
+
+let address = prompt('Address');
+
+let phone = prompt('Phone');
+
+
+let student = 'Khalid';
+
+let adress = 'Grozny Expo';
+
+let tele = 8968-888-18-17;
+
+console.log(`Студент: ${name}, Адрес: ${address}, Номер: ${phone}`);     //создал переменные
+
+
+let $ = 100;
+
+let Rub = 1000;
+
+console.log(Rub/$);
+
+
+let a = 5, b = 10, c;
+
+b = 9
+
+c = ++a + b--;
+
+console.log(c);
